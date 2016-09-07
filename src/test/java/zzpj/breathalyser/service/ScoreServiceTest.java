3f47402c65bd4721eaea361d39db7570606c5107
krzychunn <krzychu.nn@gmail.com>
2016-09-07 22:55:36 +0200

    Merge branch 'master' of F:\INF\semestr5i6\ZZPJ\repo\git\ZZPJ-2016-Breathalyser with conflicts.

---
+++ src/test/java/zzpj/breathalyser/service/ScoreServiceTest.java
@@ -0,0 +1,26 @@
+package zzpj.breathalyser.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Krzychu on 07.09.2016.
+ */
+public class ScoreServiceTest {
+    @Test
+    public void addScore() throws Exception {
+
+    }
+
+    @Test
+    public void getScores() throws Exception {
+
+    }
+
+    @Test
+    public void removeScore() throws Exception {
+
+    }
+
+}
